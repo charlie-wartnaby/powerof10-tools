c89cc7751e23dc17170180be4f1300f99b327d24
Charlie Wartnaby <charlie@wartnaby.org>
2023-06-19 21:14:19 +0100

    Starting to get some events and performances, but also a lot of unmatched tags

--- get_rankings.py
+++ get_rankings.py
@@ -32,7 +32,7 @@ def get_html_content(html_text, html_tag):
         open_match = open_regex.search(html_text, pos=offset)
         close_match = close_regex.search(html_text, pos=offset)
         if close_match is None:
-            print('Warning: no match for closing tag "f{html_tag}"')
+            print(f'Warning: no match for closing tag "{html_tag}"')
             break
         if open_match is not None and (open_match.start() < close_match.start()):
             if inside_tag_block:
@@ -62,6 +62,46 @@ def get_html_content(html_text, html_tag):
 
     return contents
 
+def debold(bold_tagged_string):
+    return bold_tagged_string.replace('<b>', '').replace('</b>', '')
+
+def process_one_rankings_table(rows):
+    state = "seeking_title"
+    row_idx = 0
+    while True:
+        if row_idx >= len(rows): return
+        
+        cells = get_html_content(rows[row_idx].inner_text, 'td')
+
+        if state == "seeking_title":
+            if 'class' not in rows[row_idx].attribs or rows[row_idx].attribs['class'] != 'rankinglisttitle':
+                pass
+            else:
+                event_title = debold(cells[0].inner_text).strip()
+                print(event_title)
+                state = "seeking_headings"
+        elif state == "seeking_headings":
+            if 'class' not in rows[row_idx].attribs or rows[row_idx].attribs['class'] != 'rankinglistheadings':
+                pass
+            else:
+                heading_idx = {}
+                for i, cell in enumerate(cells):
+                    heading = debold(cell.inner_text)
+                    heading_idx[heading] = i
+                state = "seeking_results"
+        elif state == "seeking_results":
+            if 'class' not in rows[row_idx].attribs or not rows[row_idx].attribs['class'].startswith('rlr'):
+                state = "seeking_title"
+            else:
+                name = cells[heading_idx['Name']].inner_text
+                perf = cells[heading_idx['Perf']].inner_text
+                print(name, perf)
+                pass
+        else:
+            # unknown state
+            state = "seeking_title"
+        row_idx += 1
+
 def process_one_year_gender(club_id, year, gender):
 
     request_params = {'clubid' : str(club_id),
@@ -83,8 +123,18 @@ def process_one_year_gender(club_id, year, gender):
     for table in tables:
         nested_tables = get_html_content(table.inner_text, 'table')
         second_level_tables.extend(nested_tables)
+    
+    for table in second_level_tables: # table of interest always a child table?
+        rows = get_html_content(table.inner_text, 'tr')
+        if len(rows) < 3:
+            continue
+        if 'class' not in rows[0].attribs or rows[0].attribs['class'] != 'rankinglisttitle':
+            continue
+        if 'class' not in rows[1].attribs or rows[1].attribs['class'] != 'rankinglistheadings':
+            continue
+        # Looks like we've found the table of results
+        process_one_rankings_table(rows)
 
-    pass
 
 def main(club_id=238):
     for year in range(2005,2006):
